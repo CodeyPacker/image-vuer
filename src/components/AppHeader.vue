dd85d88359962caf0c03887d42a3e8bcf959fe5a
CodeyPacker <codeypacker@gmail.com>
2020-06-07 23:47:34 -0600

    Added and styled logged out nav items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,12 +4,15 @@
       Image Storage
     </a>
     <div class="right menu">
-    <div v-if="isLoggedIn">
-      Logout
-    </div>
-    <a v-else href="#" class="ui item" @click="login">
-      Login
-    </a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <a href="#" class="item" @click="login">Galleries</a>
+        <a href="#" class="item" @click="login">Upload</a>
+        <a href="#" class="item" @click="login">Logout</a>
+      </div>
+
+      <a v-else href="#" class="ui item" @click="login">
+        Login
+      </a>
     </div>
   </div>
 </template>
@@ -32,3 +35,9 @@ export default {
 //  }
 
 </script>
+
+<style scoped>
+.horizontal {
+  display: flex;
+}
+</style>
