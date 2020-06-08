851f0eea710ff6a67aa36953cb920e793ba44f5f
CodeyPacker <codeypacker@gmail.com>
2020-06-07 22:35:03 -0600

    Changed Router from hash to browser router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
 
 Vue.use(VueRouter);
 
 // Tell it about the different routes in the app
 const router = new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/oath2/callback', component: AuthHandler }
   ]
