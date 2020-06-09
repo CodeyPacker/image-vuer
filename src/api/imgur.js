60558a475811328f26a3920e127754c5711be5d7
CodeyPacker <codeypacker@gmail.com>
2020-06-08 22:21:02 -0600

    Set up the Axios request to get user images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,6 +1,7 @@
 // This file is for network request things so they don't get
 // duplicated in the Vuex modules
 import qs from 'qs'
+import axios from 'axios'
 
 const CLIENT_ID = 'fd0395dfbb4309f'
 const ROOT_URL = 'https://api.imgur.com'
@@ -13,5 +14,16 @@ export default {
     }
 
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`
+    },
+
+    fetchImages(token) {
+      // Axios Notes:
+      // 2nd param is an object that can have a collection of options to customize reqs
+      return axios.get(`${ROOT_URL}/3/account/me/images`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+    }
   }
 }
